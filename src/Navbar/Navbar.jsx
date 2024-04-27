f9294218f35eeeca9ebf46aac967b8cd6c83897b
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-04-27 23:58:09 +0600

    added login without responsive

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100 md:p-10 font-lato">
+    <div className="navbar bg-base-100 md:px-16 py-10 font-lato">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- src/Pages/LogIn.jsx
+++ src/Pages/LogIn.jsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+import login from '../../public/images/login.jpg'
 const LogIn = () => {
   return (
-    <div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="md:px-16 ">
+      <div className="hero min-h-screen bg-base-200 font-poppins rounded-lg">
+        <div className="hero-content flex-col lg:flex-row-reverse gap-24">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
+            <h1 className="text-5xl font-poppins font-bold text-[#253b34] md:mb-4">Login now!</h1>
+            <p className="py-6 text-[18px]">
             Explore the timeless elegance of jute and wooden crafts at our website. From exquisite home decor to eco-friendly accessories, discover artisanal pieces that blend sustainability with style. Sign in now to unlock exclusive offers and indulge in the beauty of natural materials reimagined for modern living.
             </p>
           </div>
-          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card shrink-0 w-full max-w-xl shadow-2xl bg-base-100">
             <form className="card-body">
               <div className="form-control">
                 <label className="label">
@@ -32,15 +34,11 @@ const LogIn = () => {
                   className="input input-bordered"
                   required
                 />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
               </div>
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Login</button>
               </div>
+              <p className="text-center mt-4">Do not have an account? <Link to="/register" className="text-primary font-bold">Register</Link></p>
             </form>
           </div>
         </div>
