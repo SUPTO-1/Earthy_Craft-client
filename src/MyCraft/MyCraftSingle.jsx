b4f6ef51303126c4a8846190df12d6046d530673
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-04-28 23:26:33 +0600

    complete my craft and list with delete and update

--- src/MyCraft/MyCraftSingle.jsx
+++ src/MyCraft/MyCraftSingle.jsx
@@ -1,10 +1,50 @@
-const MyCraftSingle = ({craft}) => {
-    const { _id, name, email, itemName, subcategoryName, price, rating, customization, time, description, photo, stock } = craft;
-    return (
-        <div>
-            <h2>Name: {name}</h2>
+import { FaStar } from "react-icons/fa6";
+import { TbCoinTakaFilled } from "react-icons/tb";
+
+const MyCraftSingle = ({ craft }) => {
+  const {
+    itemName,
+    price,
+    rating,
+    customization,
+    photo,
+    stock,
+  } = craft;
+  return (
+    <div className="card rounded-lg bg-base-100 shadow-lg border-2">
+      <div className="card-body mb-2">
+        <img className="h-[400px] rounded-lg" src={photo} alt="" />
+      </div>
+      <div className="p-8">
+        <h2 className="text-2xl text-center font-poppins font-bold mb-6 text-[#3A4256]">
+          {itemName}
+        </h2>
+        <div className="flex justify-between mb-10">
+          <h2 className="text-xl text-[#3A4256] font-lato font-bold">
+            Price:{" "}
+            <TbCoinTakaFilled className="inline text-3xl text-[#3A4256]" />{" "}
+            {price}
+          </h2>
+          <h2 className="text-2xl text-[#3A4256] font-lato font-bold">
+            {" "}
+            {rating} <FaStar className="-mt-2 inline text-2xl text-[#3A4256]" />
+          </h2>
         </div>
-    );
+        <div className="flex justify-between">
+          <h2 className="text-xl text-[#3A4256] font-lato font-bold">
+            Customization : {customization}
+          </h2>
+          <h2 className="text-xl text-[#3A4256] font-lato font-bold">
+            Stock: {stock}
+          </h2>
+        </div>
+        <div className="flex justify-between mt-6">
+          <button className="btn bg-slate-700 text-white ">Delete</button>
+          <button className="btn bg-slate-700 text-white ">Update</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyCraftSingle;
